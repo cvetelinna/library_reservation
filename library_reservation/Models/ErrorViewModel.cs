2141ec73c82a513fe0db7cca216ef813c1462352
Boris Lachev <boris.lachevbg00@gmail.com>
2021-12-05 13:30:03 +0200

    Initial commit

---
+++ library_reservation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace library_reservation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
