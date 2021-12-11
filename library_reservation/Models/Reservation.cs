42c4beb9dce8e3de2bd1ead2a8f802272668e999
GeorgiYankov00 <georgi.yankov00@gmail.com>
2021-12-11 19:50:08 +0200

    Added Reservation props

--- library_reservation/Models/Reservation.cs
+++ library_reservation/Models/Reservation.cs
@@ -6,10 +6,26 @@
 
         public int HallId { get; set; }
 
+        public int UserId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Subject { get; set; }
+        
+        public string[] Organizers { get; set; }
+
+        public string Description { get; set; }
+        
+        public bool RequiresMultimedia { get; set; }
+
         public int ReservationRequestId { get; set; }
         
         public Hall Hall { get; set; }
 
         public ReservationRequest ReservationRequest { get; set; }
+
+        //To Do add user entity when Identity is done
     }
 }
