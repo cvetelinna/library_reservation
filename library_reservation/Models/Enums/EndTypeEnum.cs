527ae0384653454cb6741eaa6cefae06e7e4af58
LuboN00 <lubo.nedeqlkov00@gmail.com>
2021-12-28 18:06:39 +0200

    Adds Values to Enums

--- library_reservation/Models/Enums/EndTypeEnum.cs
+++ library_reservation/Models/Enums/EndTypeEnum.cs
@@ -2,8 +2,8 @@ namespace library_reservation.Models
 {
     public enum EndTypeEnum
     {
-        None,
-        Date,
-        Count
+        None = 1,
+        Date = 2,
+        Count = 3
     }
 }
--- library_reservation/Models/Enums/RecurringTypeEnum.cs
+++ library_reservation/Models/Enums/RecurringTypeEnum.cs
@@ -1,10 +1,10 @@
-
+using System.ComponentModel.DataAnnotations;
 
 namespace library_reservation.Models
 {
     public enum RecurringTypeEnum
     {
-        Weekly,
-        Monthly
+        Weekly = 1,
+        Monthly = 2
     }
 }
